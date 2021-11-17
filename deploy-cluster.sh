30e56ce0fdaea28e00d110df1725a997195953c6
Nolan <nolancrooks@outlook.com>
2021-11-17 21:42:04 +0000

    update script

--- deploy-cluster.sh
+++ deploy-cluster.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 kubectl create namespace app
-kubectl apply -f web-app/deploy.yml --namespace app
-kubectl apply -f web-app/svc-lb.yml --namespace app
-
+kubectl apply -f web-app/ --namespace app
 kubectl apply -f kube-state-metrics/
 
