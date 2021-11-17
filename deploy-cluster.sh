21288d0bca44d61eec11069c0cce49bf696a4beb
nolan crooks <noln@pop-os.localdomain>
2021-11-17 10:47:38 -0800

    update script

--- deploy-cluster.sh
+++ deploy-cluster.sh
@@ -4,5 +4,5 @@ kubectl apply -f web-app/deploy.yml --namespace monitoring
 kubectl apply -f web-app/svc-lb.yml --namespace monitoring
 
 git clone https://github.com/kubernetes/kube-state-metrics.git
-kubectl apply -f kube-state-metrics/examples/standard --namespace monitoring
+kubectl apply -f kube-state-metrics/examples/standard
 
